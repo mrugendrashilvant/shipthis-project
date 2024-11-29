48ec0f18529170f7022d0d2494ab3e5e2728f7c2
Mrugendra Shilvant <mrugendra.shilvant@techuplabs.com>
2024-11-29 16:32:03 +0530

    firebase added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideHttpClient} from "@angular/common/http";
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {environment} from "../environment/environment";
+import {FIREBASE_OPTIONS} from "@angular/fire/compat";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}),
+    provideHttpClient(),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig}
+  ]
 };
---
+++ src/environment/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    "projectId": "shipthis-a000e",
+    "appId": "1:1001165257911:web:74c24817ae072ff072e132",
+    "storageBucket": "shipthis-a000e.firebasestorage.app",
+    "apiKey": "AIzaSyDI-FnBdF4x0fRxUq-BfpqUFBJUZuPls5Q",
+    "authDomain": "shipthis-a000e.firebaseapp.com",
+    "messagingSenderId": "1001165257911"
+  }
+}
